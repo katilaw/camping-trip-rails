b3c81e654053c39e522689979101f821d4fe7879
Lawrence Kati <katilaw@gmail.com>
2015-12-16 09:39:20 -0500

    Initial Commit

---
+++ app/controllers/campers_controller.rb
@@ -0,0 +1,12 @@
+class CampersController < ApplicationController
+  def index
+    if params[:campsite_id].nil?
+      @title = "All Campers:"
+      @campers = Camper.all
+    else
+      @campsite = Campsite.find(params[:campsite_id])
+      @campers = @campsite.campers
+      @title = "Campers bunking in #{@campsite.name}"
+    end
+  end
+end
